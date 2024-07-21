1eaca30e98349711fa539b94b160502671a6fb62
Kishor20003 <kishor24610@gmail.com>
2024-07-21 11:06:12 +0530

    added js

---
+++ product.js
@@ -0,0 +1,36 @@
+//selecting side navbar
+
+var sidenav=document.getElementById("sidenav")
+var menuicon=document.getElementById("menuicon")
+var closenav=document.getElementById("close-nav")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
+
+//product search functionality
+
+var productContainer =document.getElementById("productcontainer")
+
+var search=document.getElementById("search")
+ var productlist=productContainer.querySelectorAll("div")
+
+ search.addEventListener("keyup",function(){
+    var enteredvalue=event.target.value.toUpperCase()
+
+    for(count=0;count<productlist.length;count=count+1)
+    {
+        var productname=productlist[count].querySelector("h1").textContent
+        if(productname.toUpperCase().index0f(enteredvalue)<0)
+        {
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+ })
